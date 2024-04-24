f3828584f9170a22068d36ab475943c568faffbb
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-04-24 02:52:25 +0100

    Refactored Question.js to use QuestionContent component to display content

--- src/Question.js
+++ src/Question.js
@@ -1,9 +1,10 @@
 import OptionButton from './OptionButton.js';
-const szQuestionText = "Is this topspin or backspin?"
+import QuestionContent from './QuestionContent.js';
+const szQuestionText = "Is this topspin or backspin?";
 export default function Question() {
     return (<div>
         <h2>{szQuestionText}</h2>
-        <p><iframe width="400" height="500" src="https://www.youtube.com/embed/ZK_vz4FiJuc" title=":o" allowfullscreen></iframe></p>
+        <QuestionContent videoURL={"https://www.youtube.com/embed/ZK_vz4FiJuc"} />
         <OptionButton text="topspin" />
         <OptionButton text="backspin" />
     </div>);
