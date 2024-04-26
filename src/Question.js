19e65b53038f4963690f96e538ae62a6154fff36
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-04-27 00:53:17 +0100

    Reformatting Question.js - Now takes an object containing Question data from which it reads the video URL used to display question content

--- src/Question.js
+++ src/Question.js
@@ -1,9 +1,10 @@
 import OptionButton from './OptionButton.js';
 import QuestionContent from './QuestionContent.js';
-export default function Question({szQuestionText,szVideoURL}) {
+
+export default function Question({szQuestionText,oQuestionData}) {
     return (<div>
         <h2>{szQuestionText}</h2>
-        <QuestionContent szVideoURL={szVideoURL} />
+        <QuestionContent szVideoURL={oQuestionData.szVideoURL} />
         <OptionButton text="topspin" isAnswerCorrect="false" />
         <OptionButton text="backspin" isAnswerCorrect="true" />
     </div>);
