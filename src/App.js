7ef30ee0c803c2a188b7d989f06632a75e4d3d1b
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-04-27 01:18:03 +0100

    Reformatted App.js - Now passes an example hardcoded question data object to Question component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Question from './Question.js';
+let oExampleQuestionData = {
+  szVideoURL: "https://www.youtube.com/embed/ZK_vz4FiJuc"
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Question 
         szQuestionText="Is this topspin or backspin?"
-        szVideoURL="https://www.youtube.com/embed/ZK_vz4FiJuc"
+        oQuestionData={oExampleQuestionData}
         />
       </header>
     </div>
